558f7b795d6249675701d459b800910753a3b6d3
SurFace81 <sur.f4ce@yandex.ru>
2023-11-07 06:15:01 +0300

    Добавьте файлы проекта.

---
+++ Ball.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadBalls
+{
+    internal class Ball
+    {
+        // Properties to store ball's position, velocity, acceleration, radius, and movement status
+        public int x, y;
+        public int velocityX, velocityY;
+        public int accelX, accelY;
+        public int rad;
+        public bool isMoving;
+        public int WIDTH, HEIGHT;
+
+        // Constructor to initialize ball properties
+        public Ball(int x, int y, int speedX, int speedY, int accelX, int accelY, int radius)
+        {
+            this.x = x;
+            this.y = y;
+            this.velocityX = speedX;
+            this.velocityY = speedY;
+            this.rad = radius;
+            this.accelX = accelX;
+            this.accelY = accelY;
+        }
+
+        // Method to set ball's movement status and environment dimensions
+        public void setEnv(bool isMoving, int width, int height)
+        {
+            this.isMoving = isMoving;
+            this.WIDTH = width;
+            this.HEIGHT = height;
+        }
+
+        // Method to simulate ball movement
+        public void move(int n, Form form)
+        {
+            // Continuously update ball's position while it is moving
+            while (isMoving)
+            {
+                // Update velocity based on acceleration
+                velocityX += accelX;
+                velocityY += accelY;
+
+                // Update ball's position based on velocity
+                x += velocityX;
+                y += velocityY;
+
+                // Check if the ball hits the boundaries, reverse its velocity if so
+                if (x + rad > WIDTH || x - rad < 0)
+                {
+                    velocityX = -velocityX;
+                }
+                if (y + rad > HEIGHT || y - rad < 0)
+                {
+                    velocityY = -velocityY;
+                }
+
+                // Request form repainting to reflect updated ball position
+                form.Invalidate();
+
+                // Introduce a small delay for smoother animation (10 milliseconds)
+                Thread.Sleep(10);
+            }
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ThreadBalls
+{
+    public partial class Form1 : Form
+    {
+        // Constants and variables declaration
+        private const int ballsCntr = 10;
+        private Ball[] balls = new Ball[ballsCntr];
+        private Thread[] threads = new Thread[ballsCntr];
+
+        // Constructor for the main form
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Enable double buffering for smooth rendering
+            this.DoubleBuffered = true;
+
+            // Randomly initialize balls with positions, velocities, and radius
+            Random rnd = new Random();
+            for (int i = 0; i < balls.Length; i++)
+            {
+                balls[i] = new Ball(rnd.Next(30, ClientSize.Width - 30), rnd.Next(30, ClientSize.Height - 30),
+                                    rnd.Next(1, 5), rnd.Next(1, 5),
+                                    0, 0, 15);
+
+                // Set environment for each ball and create corresponding threads
+                balls[i].setEnv(true, ClientSize.Width, ClientSize.Height);
+                int index = i;
+                threads[i] = new Thread(() => balls[index].move(index, this));
+            }
+
+            // Start threads to move balls
+            foreach (Thread t in threads)
+            {
+                t.Start();
+            }
+        }
+
+        // Override OnPaint method to draw balls on the form
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            // Brush for drawing balls
+            Brush brush = new SolidBrush(Color.FromArgb(125, 125, 125));
+
+            // Draw each ball on the form
+            for (int i = 0; i < balls.Length; i++)
+            {
+                e.Graphics.FillEllipse(brush, balls[i].x - balls[i].rad,
+                                              balls[i].y - balls[i].rad,
+                                              2 * balls[i].rad,
+                                              2 * balls[i].rad);
+            }
+        }
+
+        // Override OnFormClosing method to stop ball movement threads upon form closing
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+
+            // Stop ball movement by setting isMoving to false for each ball
+            foreach (Ball b in balls)
+            {
+                b.isMoving = false;
+            }
+
+            // Wait for all threads to finish before closing the form
+            foreach (Thread t in threads)
+            {
+                t.Join();
+            }
+        }
+    }
+}
